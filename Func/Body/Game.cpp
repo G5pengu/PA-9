1fbec1124c693beaae01545b5a6092217bfe7eca
NicholasH2 <nicholas.horner@wsu.edu>
2026-04-29 20:24:34 -0700

    Help

--- Func/Body/Game.cpp
+++ Func/Body/Game.cpp
@@ -101,7 +101,6 @@ void Game::update(float dt) {
         bgFrame = (bgFrame + 1) % 2;
     }
 
-    // Spawn arrows
     spawnTimer += dt;
     if (spawnTimer >= 1.2f) {
         spawnTimer = 0.f;
--- Func/Body/net.cpp
+++ Func/Body/net.cpp
@@ -15,7 +15,7 @@ Network::~Network()
 // UDP Send and Receive the leader board
 void Network::send()
 {
-	IpAddress ip = *IpAddress::resolve(this->ip);
+	sf::IpAddress ip = sf::Dns::resolve(this->ip).value()[0];
 	UdpSocket sock;
 	Packet pack;
 	for (int i = 0; i < 10; i++)
@@ -153,7 +153,7 @@ void Network::cat()// pure debug test case, UwU
 	cout << "strating listener thread\n";
 	thread listener(&Network::listen, this);
 
-	sleep(seconds(1));
+	sleep(seconds(60));
 
 	cout << "starting sender tread";
 	thread sender(&Network::send, m);
