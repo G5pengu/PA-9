8ae00a3b6427027c3fd94b8e6626c8444b129915
ZacCalvert <zaccalvert22@gmail.com>
2026-05-01 18:32:18 -0700

    the full test furnctions

---
+++ TestCases.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+//Name: Zac Calvert
+//Date: 4/29/2026
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Func/Header/net.hpp"
+#include "Func/Header/SoundManager.hpp"
+#include "Func/Header/NoteTimeCalc.hpp"
+#include "Func/Header/Button.hpp"
+#include "Func/Header/Arrow.hpp"
+#include "Func/Header/Game.hpp"
+
+class TestCases : public Network
+{
+public:
+	TestCases();
+
+	bool init();
+	void net();
+	bool input();
+	bool arrowSpawn();
+	bool windowTest();
+};
+
